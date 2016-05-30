431440b6accba71b5c2d719025dcec73a9489bc5
MatthiasLeroyEPFL <matthias.leroy@epfl.ch>
2016-05-30 18:20:20 +0200

    Fix bugs

--- convert2Ascii.m
+++ convert2Ascii.m
@@ -1,19 +1,21 @@
-function x = convert2Ascii(freqencies)
+function x = convert2Ascii(frequencies)
 
 if(mod(size(frequencies),4) ~= 0)
     disp('Error: some tuples are missing');
 end
 word ='';
-for n = 1:4:size(frequencies)
-    tuple1 = decode_frequencies(frequencies(n));
-    tuple2 = decode_frequencies(frequencies(n+1));
-    tuple3 = decode_frequencies(frequencies(n+2));
-    tuple4 = decode_frequencies(frequencies(n+3));
+
+for n = 1:4:size(frequencies(:))
+    n
+    tuple1 = decode_frequency(frequencies(n));
+    tuple2 = decode_frequency(frequencies(n+1));
+    tuple3 = decode_frequency(frequencies(n+2));
+    tuple4 = decode_frequency(frequencies(n+3));
     
     binaryLetter = strcat(tuple1,tuple2,tuple3,tuple4);
     
-    decimalLetter = bin2dec(binaryLetter);
-    letter = double(num2str(decimalLetter));
+    
+    letter = char(bin2dec(binaryLetter));
     word = strcat(word,letter);
 end
 
